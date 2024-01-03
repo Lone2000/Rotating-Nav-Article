51e17eeea33ca9c42334f7646cb441a5fefceaf7
Irfan <lone0059@gmail.com>
2024-01-03 21:38:07 +0500

    Rotation animation completed

---
+++ scripts.js
@@ -0,0 +1,49 @@
+function showNav() {
+  // Get the container element
+  const container = document.querySelector(".content");
+  const btn_open = document.querySelector("#open");
+  // Toggle the 'shownav' class on the container
+  container.classList.toggle("shownav");
+  //   grab nav
+  const nav = document.querySelector("nav");
+
+  // toggle inactive
+  nav.classList.remove("inactive");
+  // Toggle animation
+  nav.classList.toggle("move-in");
+
+  // Get the close button
+  const btn_close = document.querySelector("#close");
+
+  // Toggle vanish
+  btn_open.classList.toggle("vanish");
+
+  // Make Bars vanish
+  //   btn_open.classList.add("vanish");
+
+  // Make the X appear
+  btn_close.classList.remove("inactive");
+  btn_close.classList.add("active");
+
+  //   btn.close.classList.toggle("inactive");
+  //   btn_close.classList.toggle("a")
+}
+
+function hideNav() {
+  const container = document.querySelector(".content");
+  const btn_open = document.querySelector("#open");
+  const btn_close = document.querySelector("#close");
+
+  // Remove 'shownav' class on the container
+  container.classList.toggle("shownav");
+
+  // Remove 'vanish' from 'open' and add to 'close'
+  btn_open.classList.toggle("vanish");
+  //   btn_close.classList.add("vanish");
+  btn_close.classList.toggle("vanish");
+  btn_close.classList.toggle("active");
+
+  //   grab nav
+  const nav = document.querySelector("nav");
+  nav.classList.toggle("inactive");
+}
